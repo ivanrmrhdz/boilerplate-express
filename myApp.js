f2486f2c3794b2715a0c16f23a5387e2fe21be5b
Ivan Romero <ivanrmrhdz@gmail.com>
2024-10-16 21:22:46 +0000

    1.2

--- myApp.js
+++ myApp.js
@@ -1,12 +1,36 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+ 
+    res.sendFile(__dirname + "/views/index.html")
+   
+})
+
+app.get("/json",(req, res) => {
+    
+console.log(process.env.MESSAGE_STYLE)
+
+if(process.env.MESSAGE_STYLE == 'uppercase'){
+    res.json({
+        "message": "HELLO JSON"
+    })
+}else{
+    res.json({
+        "message": "Hello json"
+    })
+}
+    
+}
+
+)
+
+
 console.log("Hello World")
-
-
-
-
-
 
 
 
